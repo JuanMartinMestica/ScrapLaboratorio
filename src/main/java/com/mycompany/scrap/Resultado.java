a32d1adff677209f33218b10ea6bebc250470d3c
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-09-15 20:15:27 -0300

    Clase terminada

--- src/main/java/com/mycompany/scrap/Resultado.java
+++ src/main/java/com/mycompany/scrap/Resultado.java
@@ -5,10 +5,31 @@
  */
 package com.mycompany.scrap;
 
-/**
- *
- * @author MARTIN
- */
 public class Resultado {
     
+    private String nombreMoneda;
+    private String precioMoneda;
+
+    public Resultado(String nombreMoneda, String precioMoneda) {
+        this.nombreMoneda = nombreMoneda;
+        this.precioMoneda = precioMoneda;
+    }
+
+    public void setNombreMoneda(String nombreMoneda) {
+        this.nombreMoneda = nombreMoneda;
+    }
+
+    public void setPrecioMoneda(String precioMoneda) {
+        this.precioMoneda = precioMoneda;
+    }
+
+    public String getNombreMoneda() {
+        return nombreMoneda;
+    }
+
+    public String getPrecioMoneda() {
+        return precioMoneda;
+    }
+    
+  
 }
