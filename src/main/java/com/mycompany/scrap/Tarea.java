8643006405287c7379216cc24f4381f124a9c155
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-09-15 20:48:44 -0300

    Modificación de variables

--- src/main/java/com/mycompany/scrap/Tarea.java
+++ src/main/java/com/mycompany/scrap/Tarea.java
@@ -11,46 +11,38 @@ import java.util.concurrent.Callable;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 public class Tarea implements Callable<Resultado> {
 
     private final String url = "https://www.conversormonedas.com/valor-peso-argentino.php";
+    private final Connection conexion = Jsoup.connect(url);
+    private final Document html = conexion.get();
     private final String tagPrecio;
     private List<String> titulos;
     private final int numeroTarea;
     private final String tagMoneda;
-    private final Connection conexion;
-    private final Document html;
+
 
     //Se crea constructor y se conecta tarea a la url de la página
     public Tarea(int numeroTarea, String tagMoneda, String tagPrecio) throws IOException {
         this.tagPrecio = tagPrecio;
         this.numeroTarea = numeroTarea;
         this.tagMoneda = tagMoneda;
-        this.conexion = Jsoup.connect(url);
-        this.html = conexion.get();
     }
 
     @Override
     public Resultado call() throws Exception {
 
         System.out.println(Thread.currentThread().getName() + " Empezando tarea: " + numeroTarea);
-        
+
         //Se obtienen los nombre y precios de las monedas correspondientes
         String moneda = html.select(tagMoneda).toString();
         String precio = html.select(tagPrecio).toString();
-        
-        
-        moneda = moneda.substring(moneda.indexOf(">")+1, moneda.indexOf("</"));
-        precio = precio.substring(precio.indexOf(">")+1, precio.indexOf("</"));
-       
-        return new Resultado(moneda,precio);
-    }
-    
-       
 
-    
-    
+        moneda = moneda.substring(moneda.indexOf(">") + 1, moneda.indexOf("</"));
+        precio = precio.substring(precio.indexOf(">") + 1, precio.indexOf("</"));
+
+        return new Resultado(moneda, precio);
+    }
 
 }
